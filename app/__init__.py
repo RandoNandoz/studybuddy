b340e094d39e24f565dbd3291eda4cecea275788
Randy Zhu <mail@randyzhu.com>
2023-06-25 13:32:03 -0700

    make the database frickin work

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,19 @@ import json
 
 from flask import Flask, render_template, session, request, flash
 
+from database_stuff import db_interaction
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY="dev",
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -23,18 +26,22 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
     # TODO: add functionality lol
-    @app.route('/', methods=('GET', 'POST'))
+    @app.route("/", methods=("GET", "POST"))
     def index():
-        with open('app/static/data/data.json') as f:
-            data = json.load(f)
+        if len(session) > 0:
+            data = db_interaction.get_all_tasks(session["username"])
+        else:
+            data = {
+                "description": "No tasks found"
+            }
         # if request.method == 'POST':
-            # if request.form.get('')
-                
-        return render_template('index.html', data=data)
-    
+        # if request.form.get('')
+
+        return render_template("index.html", data=data)
+
     from . import auth
+
     app.register_blueprint(auth.bp)
     return app
- 
